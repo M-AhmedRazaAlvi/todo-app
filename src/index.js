4b2943b9be3468f14579a373fc1e53089a04994c
AhmedRazaReact <razaalvi9202@gmail.com>
2023-02-24 17:24:51 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import New from './Todo/new';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <New />
   </React.StrictMode>
 );
 
